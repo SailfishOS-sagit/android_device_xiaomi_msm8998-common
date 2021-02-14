650fd0446c72e3faa1fa361289bb892a2a53d952
Verevka <verevka86@gmail.com>
2021-02-14 23:36:58 +0300

    sagit: add firmware blobs
    * https://github.com/AOSP-11/device_xiaomi_sagit-firmware

--- msm8998.mk
+++ msm8998.mk
@@ -28,6 +28,9 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
+# Firmware
+$(call inherit-product, device/xiaomi/sagit-firmware/firmware.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
--- releasetools.py
+++ releasetools.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,52 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import hashlib
 import common
 import os
 
-TARGET_DIR = os.getenv('OUT')
+TARGET_DIR = os.getenv('OUT_DIR')
 
-# Firmware - sagit
-def FullOTA_InstallEnd(self):
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/abl.elf"), "firmware-update/abl.elf")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/BTFM.bin"), "firmware-update/BTFM.bin")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/cmnlib64.mbn"), "firmware-update/cmnlib64.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/cmnlib.mbn"), "firmware-update/cmnlib.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/devcfg.mbn"), "firmware-update/devcfg.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/hyp.mbn"), "firmware-update/hyp.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/keymaster.mbn"), "firmware-update/keymaster.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/logfs_ufs_8mb.bin"), "firmware-update/logfs_ufs_8mb.bin")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/NON-HLOS.bin"), "firmware-update/NON-HLOS.bin")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/rpm.mbn"), "firmware-update/rpm.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/storsec.mbn"), "firmware-update/storsec.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/tz.mbn"), "firmware-update/tz.mbn")
-  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/xbl.elf"), "firmware-update/xbl.elf")
+# Firmware - sagit-firmware
+def FullOTA_InstallEnd(info):
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/abl.elf"), "firmware-update/abl.elf")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/BTFM.bin"), "firmware-update/BTFM.bin")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/cmnlib64.mbn"), "firmware-update/cmnlib64.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/cmnlib.mbn"), "firmware-update/cmnlib.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/devcfg.mbn"), "firmware-update/devcfg.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/hyp.mbn"), "firmware-update/hyp.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/keymaster.mbn"), "firmware-update/keymaster.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/logfs_ufs_8mb.bin"), "firmware-update/logfs_ufs_8mb.bin")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/NON-HLOS.bin"), "firmware-update/NON-HLOS.bin")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/rpm.mbn"), "firmware-update/rpm.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/storsec.mbn"), "firmware-update/storsec.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/tz.mbn"), "firmware-update/tz.mbn")
+    info.output_zip.write(os.path.join(TARGET_DIR, "../device/xiaomi/sagit-firmware/firmware-update/xbl.elf"), "firmware-update/xbl.elf")
 
 # Write Firmware updater-script
-  self.script.AppendExtra('')
-  self.script.AppendExtra('# ---- radio update tasks ----')
-  self.script.AppendExtra('')
-  self.script.AppendExtra('ui_print("Patching firmware images...");')
-  self.script.AppendExtra('package_extract_file("firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64");')
-  self.script.AppendExtra('package_extract_file("firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib");')
-  self.script.AppendExtra('package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hyp");')
-  self.script.AppendExtra('package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");')
-  self.script.AppendExtra('package_extract_file("firmware-update/storsec.mbn", "/dev/block/bootdevice/by-name/storsec");')
-  self.script.AppendExtra('package_extract_file("firmware-update/abl.elf", "/dev/block/bootdevice/by-name/abl");')
-  self.script.AppendExtra('package_extract_file("firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg");')
-  self.script.AppendExtra('package_extract_file("firmware-update/keymaster.mbn", "/dev/block/bootdevice/by-name/keymaster");')
-  self.script.AppendExtra('package_extract_file("firmware-update/xbl.elf", "/dev/block/bootdevice/by-name/xbl");')
-  self.script.AppendExtra('package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpm");')
-  self.script.AppendExtra('package_extract_file("firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64bak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlibbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hypbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tzbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/abl.elf", "/dev/block/bootdevice/by-name/ablbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfgbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/keymaster.mbn", "/dev/block/bootdevice/by-name/keymasterbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/xbl.elf", "/dev/block/bootdevice/by-name/xblbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpmbak");')
-  self.script.AppendExtra('package_extract_file("firmware-update/logfs_ufs_8mb.bin", "/dev/block/bootdevice/by-name/logfs");')
-  self.script.AppendExtra('package_extract_file("firmware-update/NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");')
-  self.script.AppendExtra('package_extract_file("firmware-update/BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");')
+    info.script.AppendExtra('')
+    info.script.AppendExtra('# ---- radio update tasks ----')
+    info.script.AppendExtra('')
+    info.script.AppendExtra('ui_print("Patching firmware images...");')
+    info.script.AppendExtra('package_extract_file("firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64");')
+    info.script.AppendExtra('package_extract_file("firmware-update/NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");')
+    info.script.AppendExtra('package_extract_file("firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib");')
+    info.script.AppendExtra('package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hyp");')
+    info.script.AppendExtra('package_extract_file("firmware-update/BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");')
+    info.script.AppendExtra('package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");')
+    info.script.AppendExtra('package_extract_file("firmware-update/storsec.mbn", "/dev/block/bootdevice/by-name/storsec");')
+    info.script.AppendExtra('package_extract_file("firmware-update/logfs_ufs_8mb.bin", "/dev/block/bootdevice/by-name/logfs");')
+    info.script.AppendExtra('package_extract_file("firmware-update/abl.elf", "/dev/block/bootdevice/by-name/abl");')
+    info.script.AppendExtra('package_extract_file("firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg");')
+    info.script.AppendExtra('package_extract_file("firmware-update/keymaster.mbn", "/dev/block/bootdevice/by-name/keymaster");')
+    info.script.AppendExtra('package_extract_file("firmware-update/xbl.elf", "/dev/block/bootdevice/by-name/xbl");')
+    info.script.AppendExtra('package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpm");')
+    info.script.AppendExtra('package_extract_file("firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64bak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlibbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hypbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tzbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/abl.elf", "/dev/block/bootdevice/by-name/ablbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfgbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/keymaster.mbn", "/dev/block/bootdevice/by-name/keymasterbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/xbl.elf", "/dev/block/bootdevice/by-name/xblbak");')
+    info.script.AppendExtra('package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpmbak");')
